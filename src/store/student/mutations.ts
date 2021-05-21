b5579cc2710f4838134a46d4d6e07fd226700e27
ruzlicali16 <ruzlicali24@gmail.com>
2021-05-21 10:50:07 +0800

    update mutation edit student + add delete student in mutation

--- src/store/student/mutations.ts
+++ src/store/student/mutations.ts
@@ -1,26 +1,31 @@
 import { MutationTree } from 'vuex';
 import { IStudent } from './state';
 
+interface Student {
+  student: { [key: string]: string };
+  studentIndex: number;
+}
+
 const mutation: MutationTree<IStudent> = {
-  addStudent(state, payload: { [key: string]: string }) {
-    // your code
-    state.studentList.push(payload);
+  addStudent(state, student: { [key: string]: string }) {
+    state.studentList.push(student);
   },
 
   viewStudent(state, student: { [key: string]: string }) {
     state.studentInfo = student;
   },
 
-  editStudent(state, payload: { [key: string]: string }) {
-    const student = payload;
-    const studentIndex = state.studentList.findIndex(
-      (s: { [key: string]: string }) => s.id == student.id
-    );
+  editStudent(state, payload: Student) {
+    const { student, studentIndex } = payload;
     state.studentList[studentIndex] = {
       id: student.id,
       firstname: student.firstname,
       lastname: student.lastname
     };
+  },
+
+  deleteStudent(state, studentIndex: number) {
+    state.studentList.splice(studentIndex, 1);
   }
 };
 
